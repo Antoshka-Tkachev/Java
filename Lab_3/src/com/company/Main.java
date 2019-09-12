92c21bfc5d6899f006ab0ab72c166c929bfda85a
Anton <tosh234@mail.ru>
2019-09-12 12:49:36 +0300

    Lab_3

--- Lab_3/src/com/company/Main.java
+++ Lab_3/src/com/company/Main.java
@@ -3,12 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //1 часть
+        System.out.println("1 часть");
+        Dish[] arrayDish = new Dish[] { new Cup(), new Plate(), new Spoon()};
+        for(int i = 0; i < arrayDish.length; i++) {
+            System.out.print("Кухонный прибор: ");
+            arrayDish[i].info();
+            System.out.print("Миссия прбора: ");
+            arrayDish[i].mission();
+            System.out.println("");
+        }
+
+        //2 часть
+        System.out.println("2 часть");
+        Dog[] arrayDog = new Dog[] { new Pug(), new Watchdog(), new Сhihuahua()};
+        for(int i = 0; i < arrayDish.length; i++) {
+            System.out.print("Порода собаки: ");
+            arrayDog[i].breed();
+        }
+
+        //3 часть
+        System.out.println("\n3 часть");
         FurnitureShop shop = new FurnitureShop();
         Furniture stool = new Stool();
         Furniture table = new Table();
         shop.list.add(stool);
         shop.list.add(table);
         for(int i = 0; i < shop.list.size(); i++) {
+            System.out.print("Предмет мебели: ");
             shop.list.get(i).info();
         }
     }
