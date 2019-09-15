2b0cfa190a3dc5c8ec6a0de70cd2c8047200d712
Anton <tosh234@mail.ru>
2019-09-15 16:58:53 +0300

    Practical_2

---
+++ Practical_2/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name;
+        String email;
+        String gender;
+
+        System.out.print("Enter name: ");
+        name = in.nextLine();
+        System.out.print("Enter email: ");
+        email = in.nextLine();
+        System.out.print("Enter gender: ");
+        gender = in.nextLine();
+
+        Author first = new Author(name, email, gender.charAt(0));
+        System.out.println("\nInformation\n" + first.toString());
+
+        System.out.print("\nSet email: ");
+        email = in.nextLine();
+
+        System.out.println("\nNew information\n" + first.toString());
+    }
+}
