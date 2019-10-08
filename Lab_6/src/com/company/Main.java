04a921e8a713c4a0700add4b1875c04993526b41
Anton <tosh234@mail.ru>
2019-10-08 19:26:03 +0300

    Lab_6

---
+++ Lab_6/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+            MyWindow window = new MyWindow("Game");
+            window.setVisible(true);
+
+    }
+}
